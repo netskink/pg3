9f1e9f815bbf36de604507f23fd2b33683b25027
john davis <davis@doom.lan>
2023-11-01 19:46:10 -0400

    Initial Commit

---
+++ pg3/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  pg3
+//
+//  Created by john davis on 11/1/23.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
