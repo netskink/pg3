7677b9893219cd3043a6e4f31b740ff1f0caaea6
john davis <davis@doom.lan>
2023-11-02 22:17:16 -0400

    wip

---
+++ README.md
@@ -0,0 +1,6 @@
+#  pg3
+
+## urls
+
+* [storyboard mode](https://www.delasign.com/blog/xcode-storyboard-add-ui-component/)
+
--- pg3/GameScene.swift
+++ pg3/GameScene.swift
@@ -11,6 +11,17 @@ import GameplayKit
 class GameScene: SKScene {
     
     
+    let shermanHull = SKSpriteNode(imageNamed: "sherman-hull")
+    let shermanTurret = SKSpriteNode(imageNamed: "sherman-turret")
+    
+    var lastUpateTime: TimeInterval = 0
+    var dt: TimeInterval = 0
+    
+    let shermanMovePointsPerSec: CGFloat = 480.0
+    var velocity = CGPoint.zero
+
+    
+    
     override func didMove(to view: SKView) {
         // background
         let background = SKSpriteNode(imageNamed: "map3")
@@ -22,11 +33,46 @@ class GameScene: SKScene {
         // The image is 2000,2000
         // Also notice the black bars on side
         print("size: \(size.width), \(size.height)")
+        
+        // Add hull
+        shermanHull.position = CGPoint(x: size.width/4, y: size.height/4)  // position in view
+        shermanHull.anchorPoint = CGPoint(x: 0.5, y: 0.5) // default
+        shermanHull.size = CGSize(width: 411, height: 144)  // This 0.25% of original x and y
+        shermanHull.zPosition = 1
+        // The hull is added to the view
+        addChild(shermanHull)
+
+        // Add turret
+        shermanTurret.position = CGPoint(x: 0, y: 0)  // position in view
+        shermanTurret.anchorPoint = CGPoint(x: 0.49, y: 0.5) // make center a little offset in image
+        shermanTurret.size = CGSize(width: 411, height: 144)  // This 0.25% of original x and y
+        shermanTurret.zPosition = 2
+        // The turret is added to the hull, so we can move as a unit.
+        shermanHull.addChild(shermanTurret)
+        
+        
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         
+        if lastUpateTime > 0 {
+            dt = currentTime - lastUpateTime
+        } else {
+            dt = 0
+        }
+        lastUpateTime = currentTime
+        print("\(dt*1000) ms since last upate")
+        
+        moveSprite(sprite: shermanHull, velocity: CGPoint(x: shermanMovePointsPerSec, y: 0))
+    }
+    
+    func moveSprite(sprite: SKSpriteNode, velocity: CGPoint) {
+        let amountToMove = CGPoint(x: velocity.x * CGFloat(dt), y: velocity.y * CGFloat(dt))
+        print("Amount to move: \(amountToMove)")
+        
+        sprite.position = CGPoint(x: sprite.position.x + amountToMove.x,
+                                  y: sprite.position.y + amountToMove.y)
     }
 }
